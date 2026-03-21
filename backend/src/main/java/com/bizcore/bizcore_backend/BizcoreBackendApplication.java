6f531642ef0b0482d79ca0efd61271fa59f7734b
Chris Manuel <etchomechris2000@gmail.com>
2026-03-21 01:23:35 +0100

    chore(backend): initialize Spring Boot with PostgreSQL connection

---
+++ backend/src/main/java/com/bizcore/bizcore_backend/BizcoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bizcore.bizcore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizcoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizcoreBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bizcore/bizcore_backend/BizcoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizcore.bizcore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizcoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
